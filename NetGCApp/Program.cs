6691830257f4a639dfd1afdcbbe56be9e4b94e35
maxefimov <efimov.academy.top@gmail.com>
2023-03-06 21:23:50 +0300

    Добавьте файлы проекта.

---
+++ NetGCApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace NetGCApp
+{
+    class BigUser : IDisposable
+    {
+        List<long> users = new List<long>(1000);
+
+        public void Dispose()
+        {
+            Console.WriteLine("User dispose");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GC.MaxGeneration);
+            Console.WriteLine(GC.GetTotalMemory(false));
+            int count = Int32.Parse(Console.ReadLine());
+
+            using (BigUser userObj = new BigUser())
+            {
+                Console.WriteLine(GC.GetGeneration(userObj));
+
+                for (int i = 0; i < count; i++)
+                {
+                    BigUser user = new();
+                }
+
+                Console.WriteLine(GC.GetTotalMemory(false));
+
+                GC.Collect(1);
+
+                Console.WriteLine(GC.GetGeneration(userObj));
+            }
+
+            
+            Console.WriteLine(GC.GetTotalMemory(false));
+
+            
+        }
+    }
+}
